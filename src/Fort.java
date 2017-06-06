fa7d4409867dfe714427ccb0353c0604574cd2fb
john-doe-123 <john.doe123@abv.bg>
2017-06-06 11:01:29 +0300

    Updated Fort.

--- src/Fort.java
+++ src/Fort.java
@@ -17,86 +17,82 @@ public class Fort {
         int roofUnderscores = width - (roofTiles * 2 + 4);
 
 
-        for (int row = 0; row < n; row++) {
+        //region First row
+        System.out.print("/");
+        for (int roofTile = 0; roofTile < roofTiles; roofTile++) {
+            System.out.print("^");
+        }
+        System.out.print("\\");
 
-            for (int col = 0; col < width; col++) {
+        for (int roofUnderscore = 0; roofUnderscore < roofUnderscores; roofUnderscore++) {
+            System.out.print("_");
+        }
 
-                if (row == 0) {
-                    System.out.print("/");
-                    for (int roofTile = 0; roofTile < roofTiles; roofTile++) {
-                        System.out.print("^");
-                    }
-                    System.out.print("\\");
+        System.out.print("/");
+        for (int roofTile = 0; roofTile < roofTiles; roofTile++) {
+            System.out.print("^");
+        }
+        System.out.print("\\");
 
-                    for (int roofUnderscore = 0; roofUnderscore < roofUnderscores; roofUnderscore++) {
-                        System.out.print("_");
-                    }
-
-                    System.out.print("/");
-                    for (int roofTile = 0; roofTile < roofTiles; roofTile++) {
-                        System.out.print("^");
-                    }
-                    System.out.print("\\");
+        System.out.println();
+        //endregion
 
 
-                    break;
-                } else if (row == n - 1) {
-
-                    System.out.print("\\");
-                    for (int roofTile = 0; roofTile < roofTiles; roofTile++) {
-                        System.out.print("_");
-                    }
-                    System.out.print("/");
-
-                    // Same number of spaces as the number of roof underscores.
-                    for (int roofUnderscore = 0; roofUnderscore < roofUnderscores; roofUnderscore++) {
-                        System.out.print(" ");
-                    }
-
-                    System.out.print("\\");
-                    for (int roofTile = 0; roofTile < roofTiles; roofTile++) {
-                        System.out.print("_");
-                    }
-                    System.out.print("/");
-
-                    System.out.println();
-                    break;
-                } else if (row == n - 2) {
-
-                    int numSpaces = (width - (roofUnderscores + 2)) / 2;
-
-                    System.out.print("|");
-                    for (int space = 0; space < numSpaces; space++) {
-                        System.out.print(" ");
-                    }
-
-                    for (int numUnderscores = 0; numUnderscores < roofUnderscores; numUnderscores++) {
-                        System.out.print("_");
-                    }
-
-                    for (int space = 0; space < numSpaces; space++) {
-                        System.out.print(" ");
-                    }
-                    System.out.print("|");
-
-                    break;
-                }   else {
-                    System.out.print("|");
-                    for (int spaces = 0; spaces < width - 2; spaces++) {
-                        System.out.print(" ");
-                    }
-                    System.out.print("|");
-                    break;
-                }
+        //region Middle
+        for (int i = 0; i < n - 3; i++) {
+            System.out.print("|");
+            for (int spaces = 0; spaces < width - 2; spaces++) {
+                System.out.print(" ");
             }
+            System.out.print("|");
 
             System.out.println();
-
-
         }
+        //endregion
+
+
+        //region Second to last
+        int numSpaces = (width - (roofUnderscores + 2)) / 2;
+
+        System.out.print("|");
+        for (int space = 0; space < numSpaces; space++) {
+            System.out.print(" ");
+        }
+
+        for (int numUnderscores = 0; numUnderscores < roofUnderscores; numUnderscores++) {
+            System.out.print("_");
+        }
+
+        for (int space = 0; space < numSpaces; space++) {
+            System.out.print(" ");
+        }
+        System.out.print("|");
+
+        System.out.println();
+        //endregion
+
+
+        //region Last row
+        System.out.print("\\");
+        for (int roofTile = 0; roofTile < roofTiles; roofTile++) {
+            System.out.print("_");
+        }
+        System.out.print("/");
+
+        // Same number of spaces as the number of roof underscores.
+        for (int roofUnderscore = 0; roofUnderscore < roofUnderscores; roofUnderscore++) {
+            System.out.print(" ");
+        }
+
+        System.out.print("\\");
+        for (int roofTile = 0; roofTile < roofTiles; roofTile++) {
+            System.out.print("_");
+        }
+        System.out.print("/");
+
+        System.out.println();
+        //endregion
 
 
     }
-
-
 }
